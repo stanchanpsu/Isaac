6eee118790b36356964d0a85f2f9ef505188a8b8
sman5922 <stanchanpsu@outlook.com>
2015-05-27 17:38:37 +0000

    messed with navigation

--- static/overall.js
+++ static/overall.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     // LogoExpand();
     $(".button-collapse").sideNav();
     $(".dropdown-button").dropdown();
-      $('.datepicker').pickadate({
+    $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
