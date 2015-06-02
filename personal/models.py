a553aa3ccec992c764e1f6d65db3ee70b59eaaea
stanchanpsu <stanchanpsu@outlook.com>
2015-06-02 18:19:14 -0500

    draft of profile model

--- personal/models.py
+++ personal/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from django.core.validators import RegexValidator
 
-class EngineeringAmbassador(models.Models):
+class EngineeringAmbassador(models.Model):
      user = models.OneToOneField(User)
-     picture = models.PictureField
+     picture = models.ImageField()
+     grad_date = models.DateField()
+     fall_status = models.BooleanField()
+     spring_status = models.BooleanField()
+     student_id = models.PositiveIntegerField()
+     company_designation = models.CharField(max_length = 20)
+     major = models.CharField(max_length = 20)
+     major_2 = models.CharField(max_length = 20)
+     minor = models.CharField(max_length = 20)
+     minor_2 = models.CharField(max_length = 20)
+     schreyer_honors = models.BooleanField()
+     phone = models.CharField(max_length = 10)
+     
      
