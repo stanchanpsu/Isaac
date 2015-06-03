babe35a8a0d25f813e3160d54706f29ebbeebea1
sman5922 <stanchanpsu@outlook.com>
2015-06-03 20:54:49 +0000

    started profiles

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class EngineeringAmbassador(models.Models):
+     user = models.OneToOneField(User)
+     picture = models.PictureField
+     
