2197d1436a7264a7040c5ad52e873c3176b559de
sman5922 <stanchanpsu@outlook.com>
2015-06-09 20:54:25 +0000

    finishing up personal page

--- personal/views.py
+++ personal/views.py
@@ -8,24 +8,25 @@ from itertools import chain
 
 
 @login_required(login_url='/login/')
-def profile(request):
+def profile(request, edit_status = None):
     stylesheet = 'personal/profile.css'
     app = 'personal'
     ambassador = get_object_or_404(EngineeringAmbassador, user = request.user)
     profile_pic = ambassador.picture.url
     
+    edit = False
+    
     events_registered = sorted(chain(request.user.outreach.all(), request.user.tours.all()), key=lambda instance: instance.date)
     
-    
-    # PUT THE EDIT BOOLEAN AFTER THE IF STATEMENT TO ACCOUNT FOR RENDER FROM GET
-    
-   
     if request.POST:
-        edit = True
-        
-    else:
-        edit = False       
-    
+        if edit_status == 'edit':
+            edit = True
+            return render(request, 'personal/profile.html', {'stylesheet':stylesheet, 'app': app, 'profile_pic':profile_pic, 'ambassador':ambassador, 'events_registered':events_registered, 'edit':edit,})
+            
+        else:
+            edit = False
+            
+            
     return render(request, 'personal/profile.html', {'stylesheet':stylesheet, 'app': app, 'profile_pic':profile_pic, 'ambassador':ambassador, 'events_registered':events_registered, 'edit':edit,})
         
     
