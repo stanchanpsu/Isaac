ded2ab769c9858e1ad244a9937320992aa962bcb
stanchanpsu <stanchanpsu@outlook.com>
2015-06-20 08:30:54 -0500

    got groupme auth to work

--- groupme/views.py
+++ groupme/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login/')
 def groupme(request):
     
-    if request.GET and 'access_token' in request.GET:
-        access_token = request.GET['access_token']
-    
-	return render(request, 'groupme/groupme.html', {})
+	# if groupme callbacks with the access token of the user
+	if request.GET and 'access_token' in request.GET:
+		request.session['access_token'] = request.GET['access_token']
+		access_token = request.session['access_token']
+		
+		return redirect('/groupme/')
+	
+	# if regular page request by user	
+	else:
+		# if user logged into groupme
+		if 'access_token' in request.session:
+			return render(request, 'groupme/groupme.html',{'access_token':request.session['access_token']})
+			
+		#if user is logged out of groupme
+		else:
+			auth_url = 'https://oauth.groupme.com/oauth/authorize?client_id=bnveOko8sTysD27ugGxOL5HhPeBmrxhzmXdewuXarxi50FOk'
+			return render(request, 'groupme/groupme.html',{'auth_url':auth_url,})
