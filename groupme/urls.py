a286f8a5736b3ba9d432110a867115b4051615cb
sman5922 <stanchanpsu@outlook.com>
2015-08-01 16:21:16 +0000

    groupme restructure

--- groupme/urls.py
+++ groupme/urls.py
@@ -8,4 +8,5 @@ url(r'^$', views.groupme, name = 'groupme'),
 url(r'^token', views.token, name ='token'),
 url(r'^group', views.group, name = 'group'),
 url(r'^message', views.message, name = 'message'),
+
 ]
