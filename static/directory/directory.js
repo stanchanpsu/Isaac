d0229970dab64aa5f1029bbb0658bf8f0fd5a582
stanchanpsu <stanchan@psu.edu>
2015-08-18 08:59:21 -0400

    streamlined a bunch of directory frontend

--- static/directory/directory.js
+++ static/directory/directory.js
@@ -53,29 +53,15 @@ $(function ajax_query(){
 			// for every ambassador within the object json data
 			for (i; i < object.length; i++){
 				//instantiate a column div and card div as well as the ambassador card which uses dynamic data for each person
-				var $col = $('<div>', {id: 'col' + i, class: 'col s6 l3'});
+				var $col = $('<div>', {id: 'col' + i, class: 'col s12 l2'});
 				var $card = $('<div>', {id: 'card' + i, class:'card'});
 				// note: the javascript string marker quotation must be single quote and the html quotes must be double quote because the json returned by django stores strings as double quotes
 				var ambassador_card = '<div class="card-image waves-effect waves-block waves-light"><img class="activator" src=' + object[i]['picture'] + '></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">' + object[i]['full_name'] + '<i class="mdi-navigation-more-vert right"></i></span><p>' + object[i]['major'] + '</p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + object[i]['full_name'] + '<i class="mdi-navigation-close right"></i></span><a href="/directory/' + object[i]['id'] + '" class="waves-effect waves-light btn blue">Profile<i class="mdi-social-person left"></i></a><p>' + object[i]['aboutme'] + '</p></div>';
 				
-				// if the card is not the end of a row or it is the first card
-				if (i % 4 != 0 || i == 0){
-					// just append the column, card, and ambassador card divs
-					$row.append($col);
-					$col.append($card);
-					$card.append(ambassador_card);
-				}
-				// else if the end of a column but not the first card
-				else if ( i % 4 == 0 && i !=0 ){
-					// increment the row counter
-					row_number++;
-					// redefine the row div and append it along with the column, card, and ambassador card.
-					$row = $('<div>', {id: 'row' + row_number, class: 'row'});
-					$('#grid').append($row);
-					$row.append($col);
-					$col.append($card);
-					$card.append(ambassador_card);
-				}
+				$row.append($col);
+				$col.append($card);
+				$card.append(ambassador_card);
+
 			}
 		});
 	});
